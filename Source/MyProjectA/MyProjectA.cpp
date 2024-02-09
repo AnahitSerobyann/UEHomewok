1aed6ad0c05e68c5ee43c67970cdd3beb606eccd
Your Name <you@example.com>
2024-02-09 14:32:14 +0400

    first commit

---
+++ Source/MyProjectA/MyProjectA.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MyProjectA.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyProjectA, "MyProjectA" );
